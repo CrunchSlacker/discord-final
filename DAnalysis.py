062172192e37159592716b40e85c23dcf4fb7e2a
Cyrus <cyrusyong1@gmail.com>
2021-04-15 17:06:08 -0400

    added sigfig functinality

--- DAnalysis.py
+++ DAnalysis.py
@@ -1,34 +1,63 @@
+import math
+
 import molarmass
 
 
 def DA(command):
     starting_unit = command[3]
+    starting_mol = command[2]
 
     desired_unit = command[-2]
     desired_element = command[-1]
 
-    if ":" in command[5]:
-        ratio = command[5].split(':')
-        mol_ratio = True
-    else:
-        mol_ratio = False
-
-    if starting_unit == 'g':  # Convert grams to mol
+    if starting_unit == 'g':  # Change grams to mols
         starting_mol = round(float(command[2]) / molarmass.calc_mass(command[4]), 2)
 
-    if mol_ratio:  # Multiply by mol ratio (if any)
+    if ":" in command[5]:
+        ratio = command[5].split(':')
         desired_mol = starting_mol * (int(ratio[0]) / int(ratio[1]))
-        print(desired_mol)
 
-    if desired_unit == 'g':  # Converts from mols to grams
-        if not mol_ratio:
-            final_result = starting_mol * molarmass.calc_mass(desired_element)
+        if desired_unit == 'mol':
+            final_result = desired_mol
+            sigfig = find_sigfigs(command[2])
+            final_result_sigfig = round(final_result, sigfig - int(math.floor(math.log10(abs(final_result)))) - 1)
+            return str(round(final_result_sigfig, sigfig)) + desired_unit + " of " + desired_element
 
-        if mol_ratio:
+        elif desired_unit == 'g':
             final_result = desired_mol * molarmass.calc_mass(desired_element)
+            sigfig = find_sigfigs(command[2])
+            final_result_sigfig = round(final_result, sigfig - int(math.floor(math.log10(abs(final_result)))) - 1)
+            return str(round(final_result_sigfig, sigfig)) + desired_unit + " of " + desired_element
 
-        return round(final_result, 3)
 
-    elif desired_element == 'mol':
-        final_result = desired_mol
-        return round(final_result, 3)
+"""
+Created by Bob
+https://stackoverflow.com/questions/8142676/python-counting-significant-digits
+"""
+
+
+def find_sigfigs(x):
+    """Returns the number of significant digits in a number. This takes into account
+       strings formatted in 1.23e+3 format and even strings such as 123.450"""
+    # change all the 'E' to 'e'
+    x = x.lower()
+    if 'e' in x:
+        # return the length of the numbers before the 'e'
+        myStr = x.split('e')
+        return len(myStr[0]) - 1  # to compensate for the decimal point
+    else:
+        # put it in e format and return the result of that
+        # NOTE: because of the 8 below, it may do crazy things when it parses 9 sigfigs
+        n = ('%.*e' % (8, float(x))).split('e')
+        # remove and count the number of removed user added zeroes. (these are sig figs)
+        if '.' in x:
+            s = x.replace('.', '')
+            # number of zeroes to add back in
+            l = len(s) - len(s.rstrip('0'))
+            # strip off the python added zeroes and add back in the ones the user added
+            n[0] = n[0].rstrip('0') + ''.join(['0' for num in range(l)])
+        else:
+            # the user had no trailing zeroes so just strip them all
+            n[0] = n[0].rstrip('0')
+        # pass it back to the beginning to be parsed
+    return find_sigfigs('e'.join(n))
--- main.py
+++ main.py
@@ -3,14 +3,12 @@ import molarmass
 import DAnalysis
 import os
 
-client = discord.Client()
+client = discord.Client()  # Connects with discord
 
 
 @client.event
 async def on_ready():
-    print(
-        "So this is where Enterprise is... A-ahem! I am Essex, one of the Union's state-of-the-art aircraft carriers. "
-        "Commander, you can expect great things from me! I swear that I won't disappoint you!".format(client))
+    print("Bot is ready".format(client))  # Ready message
 
 
 @client.event
@@ -18,20 +16,25 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    # If discord message begins with -e, then execute molar mass / DA command
     if message.content.startswith('-e'):
         command = message.content.split()
+        da_embed_title = command[2] + command[3] + " of " + command[4] + " to " + command[-1] + "(" + command[-2] + ")"
 
+        # Calculates molar mass and sends to user
         if command[1] == "molarM":
-            embed_molar_m = discord.Embed(title=str(command[2]) + " のモル質量:",
+            embed_molar_m = discord.Embed(title=str(command[2]) + " molar mass:",
                                           description=str(round(molarmass.calc_mass(command[2]), 2)), color=0x3498db)
-            embed_molar_m.set_thumbnail(url='https://media.tenor.com/images/dfa3ed17c46d7a679b5d2b4ae9fdca61/tenor.gif')
+
             await message.channel.send(embed=embed_molar_m)
 
-        if command[1] == "DA":
-            await message.channel.send(DAnalysis.DA(command))
+        # Calculates DA of specified elements and sends to user
+        elif command[1] == "DA":
+            embed_da = discord.Embed(title=da_embed_title, description=DAnalysis.DA(command), colour=0x3498db)
+            await message.channel.send(embed=embed_da)
 
         else:
-            await message.channel.send("有効な入力ではありません、やり直してください。")
+            await message.channel.send("Invalid Command")
 
 
 client.run('ODMxMTk3Nzc1MDg5MzY5MTA1.YHRvQA.hraUmwO2yoVfG8ekKh1NwAb4-ek')
