c4af7049f89b52819482a1cb8aad20c8cea4e388
Cyrus <cyrusyong1@gmail.com>
2021-06-10 00:42:33 -0400

    kinematics eqs test

--- main.py
+++ main.py
@@ -17,33 +17,37 @@ async def on_ready():
 async def on_message(message):
     command = message.content.split()
 
-    if command[0] == "-option":
-        optionsList = discord.Embed(title="Options", description=str("Molar Mass Calculator: \n-molar ["
-                                                                     "element/compound] \n ex. -molar H2SO4 \n\n "
-                                                                     "Dimensional Analysis: \n "
-                                                                     " -DA [amount] [unit] [e/c] [m ratio] [resulting "
-                                                                     "unit] [e/c] \n ex. -DA 12.5 g Cu2S 1:2 g Cu "
-                                                                     "\n\n Celsius to Kelvin Conversion: \n -k ["
-                                                                     "celsius] \n ex. -k 69.8"), color=0x3498db)
-        await message.channel.send(embed=optionsList)
+    try:
+        if command[0] == "-molar":
+            embedMolarM = discord.Embed(title=str(command[1]) + " のモル質量:", description=str(round(
+                molarmass.calc_mass(command[1]), 2)), color=0x3498db)
+            await message.channel.send(embed=embedMolarM)
 
-    if command[0] == "-molar":
-        embedMolarM = discord.Embed(title=str(command[1]) + " のモル質量:", description=str(round(
-            molarmass.calc_mass(command[1]), 2)), color=0x3498db)
-        await message.channel.send(embed=embedMolarM)
+        elif command[0] == "-da":
+            embedDA = discord.Embed(title="単位変換:", description=str(DAnalysis.DA(command)), color=0x3498db)
+            await message.channel.send(embed=embedDA)
 
-    elif command[0] == "-da":
-        embedDA = discord.Embed(title="単位変換:", description=str(DAnalysis.DA(command)), color=0x3498db)
-        await message.channel.send(embed=embedDA)
+        elif command[0] == "-k":
+            kelvin = float(command[1]) + 273
+            embedKelvin = discord.Embed(title="ケルビンから摂氏へ換算:", description=str(kelvin), color=0x3498db)
+            await message.channel.send(embed=embedKelvin)
 
-    elif command[0] == "-k":
-        kelvin = float(command[1]) + 273
-        embedKelvin = discord.Embed(title="ケルビンから摂氏へ換算:", description=str(kelvin), color=0x3498db)
-        await message.channel.send(embed=embedKelvin)
+        elif command[0] == "-kinematics":
+            embedKinematics = discord.Embed(title="運動方程式:", description=str(physics.solve_equation(command)),
+                                            color=0x3498db)
+            await message.channel.send(embed=embedKinematics)
 
-    elif command[0] == "-kinematics":
-        embedKinematics = discord.Embed(title="運動方程式:", description=str(physics.equation(command)), color=0x3498db)
-        await message.channel.send(embed=embedKinematics)
+        elif command[0] == "-option":
+            optionsList = discord.Embed(title="Options", description=str("Molar Mass Calculator: \n-molar ["
+                                                                         "element/compound] \n ex. -molar H2SO4 \n\n "
+                                                                         "Dimensional Analysis: \n "
+                                                                         " -DA [amount] [unit] [e/c] [m ratio] [resulting "
+                                                                         "unit] [e/c] \n ex. -DA 12.5 g Cu2S 1:2 g Cu "
+                                                                         "\n\n Celsius to Kelvin Conversion: \n -k ["
+                                                                         "celsius] \n ex. -k 69.8"), color=0x3498db)
+            await message.channel.send(embed=optionsList)
+    except IndexError:
+        pass
 
 
 client.run('ODMxMDE1MjI2MjY2MDkxNTMx.YHPFPQ.J5kobr2fp6Kj9Wz5hZj7QMzp5WA')  # Token
--- physics.py
+++ physics.py
@@ -1,10 +1,36 @@
-def equation(command):
-    print(command)
+import math
 
-    v_var = ""
-    v0_var = ""
-    a_var = ""
-    t_var = ""
+
+class AssignVar:  # Figure this out BOY
+    def __init__(self, command):
+        self.command = command
+
+    def get_var(self):
+        elem_one = ""
+        elem_two = ""
+        elem_three = ""
+        elem_four = ""
+
+        index = -1
+        for element in self.command:
+            index = index + 1
+
+            if "=" in element:
+                if index == 1:
+                    elem_one = self.command[index].split('=')
+                elif index == 2:
+                    elem_two = self.command[index].split('=')
+                elif index == 3:
+                    elem_three = self.command[index].split('=')
+                elif index == 4:
+                    elem_four = self.command[index].split('=')  #
+
+
+def no_x(command):
+    vf_elem = ""
+    vi_elem = ""
+    a_elem = ""
+    t_elem = ""
 
     index = -1
     for element in command:
@@ -12,33 +38,158 @@ def equation(command):
 
         if "=" in element:
             if index == 1:
-                v_var = command[index].split('=')
+                vf_elem = command[index].split('=')
             elif index == 2:
-                v0_var = command[index].split('=')
+                vi_elem = command[index].split('=')
             elif index == 3:
-                a_var = command[index].split('=')
+                a_elem = command[index].split('=')
             elif index == 4:
-                t_var = command[index].split('=')
+                t_elem = command[index].split('=')
 
-    v_val = v_var[1]
-    v0_val = v0_var[1]
-    a_val = a_var[1]
-    t_val = t_var[1]
+    vf_val = vf_elem[1]
+    vi_val = vi_elem[1]
+    a_val = a_elem[1]
+    t_val = t_elem[1]
 
-    if v_var[1] == "":
-        v = float(v_val) + (float(a_val) * float(t_val))
-        return "v = " + str(v)
-    if v0_var[1] == "":
-        v0 = float(v_val) / (float(a_val) * float(t_val))
-        return "v0 = " + str(v0)
-    if a_var[1] == "":
-        a = (float(v_val) - float(v0_val)) / float(t_val)
+    if vf_val == "":
+        v = float(vi_val) + (float(a_val) * float(t_val))
+        return "vf = " + str(v)
+    if vi_val == "":
+        v0 = float(vf_val) / (float(a_val) * float(t_val))
+        return "vi = " + str(v0)
+    if a_val == "":
+        a = (float(vf_val) - float(vi_val)) / float(t_val)
         return "a = " + str(a)
-    if t_var[1] == "":
-        t = (float(v_val) - float(v0_val)) / float(a_val)
+    if t_val == "":
+        t = (float(vf_val) - float(vi_val)) / float(a_val)
         return "t = " + str(t)
 
-    print(v_var)
-    print(v0_var)
-    print(a_var)
-    print(t_var)
+
+def no_a(command):
+    x_elem = ""
+    vt_elem = ""
+    v0_elem = ""
+    t_elem = ""
+
+    index = -1
+    for element in command:
+        index = index + 1
+
+        if "=" in element:
+            if index == 1:
+                x_elem = command[index].split('=')
+            elif index == 2:
+                vt_elem = command[index].split('=')
+            elif index == 3:
+                v0_elem = command[index].split('=')
+            elif index == 4:
+                t_elem = command[index].split('=')
+
+    x_val = x_elem[1]
+    vt_val = vt_elem[1]
+    v0_val = v0_elem[1]
+    t_val = t_elem[1]
+
+    if x_val == "":
+        x = 0.5 * (float(vt_val) + float(v0_val)) * float(t_val)
+        return "x = " + str(x)
+    if vt_val == "":
+        vt = 2 * (float(x_val) / float(t_val)) - float(v0_val)
+        return "vt = " + str(vt)
+    if v0_val == "":
+        v0 = 2 * (float(x_val) / float(t_val)) - float(vt_val)
+        return "v0 = " + str(v0)
+    if t_val == "":
+        t = (2 * float(x_val)) / (float(vt_val) + float(v0_val))
+        return "t = " + str(t)
+
+
+def no_vf(command):
+    x_elem = ""
+    vo_elem = ""
+    t_elem = ""
+    a_elem = ""
+
+    index = -1
+    for element in command:
+        index = index + 1
+
+        if "=" in element:
+            if index == 1:
+                x_elem = command[index].split('=')
+            elif index == 2:
+                vo_elem = command[index].split('=')
+            elif index == 3:
+                t_elem = command[index].split('=')
+            elif index == 4:
+                a_elem = command[index].split('=')
+
+    x_val = x_elem[1]
+    vo_val = vo_elem[1]
+    t_val = t_elem[1]
+    a_val = a_elem[1]
+
+    if x_val == "":
+        x = (float(vo_val) * float(t_val)) + (0.5 * float(a_val) * pow(float(t_val), 2))
+        return "x = " + str(x)
+    if vo_val == "":
+        vo = ((2 * float(x_val)) - (float(a_val) * pow(float(t_val), 2))) / (2 * float(t_val))
+        return "vo = " + str(vo)
+    if t_val == "":
+        t = (-float(vo_val) + math.sqrt(2 * float(a_val) * float(x_val) + pow(float(vo_val), 2)))
+        return "t = " + str(t)
+    if a_val == "":
+        a = ((2 * float(x_val)) - (2 * float(vo_val) * float(t_val)))
+        return "a = " + str(a)
+
+
+def no_t(command):
+    a_elem = ""
+    x_elem = ""
+    vt_elem = ""
+    v0_elem = ""
+
+    index = -1
+    for element in command:
+        index = index + 1
+
+        if "=" in element:
+            if index == 1:
+                a_elem = command[index].split('=')
+            elif index == 2:
+                x_elem = command[index].split('=')
+            elif index == 3:
+                vt_elem = command[index].split('=')
+            elif index == 4:
+                v0_elem = command[index].split('=')
+
+    a_val = a_elem[1]
+    x_val = x_elem[1]
+    vt_val = vt_elem[1]
+    v0_val = v0_elem[1]
+
+    if a_val == "":
+        a = ((pow(float(vt_val), 2) - pow(float(v0_val), 2)) / (2 * float(x_val)))
+        return "a = " + str(a)
+    if x_val == "":
+        x = ((pow(float(vt_val), 2) - pow(float(v0_val), 2)) / (2 * float(a_val)))
+        return "x = " + str(x)
+    if vt_val == "":
+        vt = math.sqrt((2 * float(a_val) * float(x_val)) + pow(float(v0_val), 2))
+        return "vt = " + str(vt)
+    if v0_val == "":
+        v0 = math.sqrt((-2 * float(a_val) * float(x_val)) + pow(float(vt_val), 2))
+        return "v0 = " + str(v0)
+
+
+def solve_equation(command):
+    print(command)
+    if 'v' in command[1]:
+        return no_x(command)
+    elif 'x' in command[1]:
+        if 'vt' in command[2]:
+            return no_a(command)
+        elif 'vo' in command[2]:
+            return no_vf(command)
+    elif 'a' in command[1]:
+        return no_t(command)
